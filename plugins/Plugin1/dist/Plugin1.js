0b5cdebaa803cf112684a67e2e850ddd8b280f30
Ivan Xu <info@chivalrystudio.com>
2020-08-25 14:23:54 -0700

    rollup done

--- plugins/Plugin1/dist/Plugin1.js
+++ plugins/Plugin1/dist/Plugin1.js
@@ -1,6 +1,16 @@
 (function () {
-	'use strict';
+    'use strict';
 
-
+    class TestClass extends Bitmap {
+        /**
+         * @param x
+         * @param y
+         */
+        testFunc(x, y) {
+            return x + y;
+        }
+    }
+    const btm = new TestClass(100, 200);
+    console.log(btm.clear());
 
 }());
