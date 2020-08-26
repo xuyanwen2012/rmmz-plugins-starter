918998fd520f3180f8dcb671a4859d86a2bdde54
Ivan Xu <info@chivalrystudio.com>
2020-08-25 18:22:08 -0700

    adding banner

--- dist/Plugin1.js
+++ dist/Plugin1.js
@@ -1,3 +1,27 @@
+//=============================================================================
+// RPG Maker MZ - Plugin1.js
+//=============================================================================
+
+/*:
+ * @target MZ
+ * @plugindesc Prototype
+ * @author Author Name
+ *
+ * @help Plugin1.js - v1.0.0
+ *
+ * @command aCommand
+ * @text A command
+ * @desc A command description.
+ *
+ * @arg anArg
+ * @type number
+ * @min 1
+ * @max 60
+ * @default 1
+ * @text An argument
+ * @desc An argument description.
+ */
+
 (function () {
     'use strict';
 
